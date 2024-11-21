3d392a6def255101a49cff21da5aed269f2a7619
hheitgre <hheitgre@hs-mittweida.de>
2024-11-21 11:10:40 +0100

    Added animations to the tutorial text and wrote useful placeholder text for level 1

--- BobDerKleinkriminelle/Assets/Scripts/TutorialManager.cs
+++ BobDerKleinkriminelle/Assets/Scripts/TutorialManager.cs
@@ -4,9 +4,9 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
-public class TutorialManager : MonoBehaviour
-{
+public class TutorialManager : MonoBehaviour{
     public GameObject tutorialCanvas;
     public Button tutorialCloseButton;
     public TextMeshProUGUI tutorialText;
@@ -33,17 +33,47 @@ public class TutorialManager : MonoBehaviour
     public void SetTutorialText(){
         Scene currentScene = SceneManager.GetActiveScene();
         Debug.Log($"[SetTutorialText] Current Scene is: {currentScene.name}");
+        string tutorialMessage = "";
 
         switch(currentScene.name){
             case "Level1":
-                tutorialText.text = "Oft ist es nicht möglich, Objekte zu teilen. Das nennt man 0/1 Problem.";
+                tutorialMessage = Level_1_Message;
                 break;
             case "Level2":
-                tutorialText.text = "Das hier ist ein Platzhaltertext für die Einführung in Level 2.";
+                tutorialMessage = "Das hier ist ein Platzhaltertext für die Einführung in Level 2.";
                 break;
             default:
                 Debug.LogError($"Unknown scene: {currentScene.name}");
                 break;
         }
+
+        //StartCoroutine(AnimateText(tutorialMessage, 0.075f));
+        StartCoroutine(AnimateText(tutorialMessage, 0.0001f));
     }
+
+    private IEnumerator AnimateText(string message, float delay){
+        tutorialText.text = "";
+        foreach(char letter in message.ToCharArray())
+        {
+            tutorialText.text += letter;
+            if(letter == ' ' || letter == '\n'){
+                continue;
+            }
+            yield return new WaitForSeconds(delay);
+        }
+    }
+
+    private String Level_1_Message = "Bei dem Knapsack- bzw. Rucksackproblem geht es darum, eine Auswahl " + 
+    "von Objekten mit bestimmten Gewichten und Werten in einen Rucksack mit begrenzter Tragfähigkeit zu packen." + 
+    " Ziel ist es, den Gesamtwert der ausgewählten Objekte zu maximieren, ohne das Gewichtslimit zu " + 
+    "überschreiten.\n\nIm 0/1 Knapsack-Problem sind die Objekte nicht teilbar, was bedeutet, dass man jedes " + 
+    "Objekt entweder vollständig in den Rucksack packt oder gar nicht. Du wirst zur Lösung den Greedy-Algorithmus " + 
+    "und die dynamische Programmierung kennenlernen.\n\n Beim Greedy-Algorithmus berechnest du das " + 
+    "Wert-zu-Gewicht-Verhältnis, genau wie das Preis-Leistungs-Verhältnis bei Lebensmitteln. Es wird solange " + 
+    "der Rucksack mit den günstigsten Objekten gefüllt, bis das nächste nicht mehr passt. Dieser Ansatz führt " + 
+    "jedoch nicht immer zur optimalen Lösung, da er nicht alle möglichen Kombinationen berücksichtigt.\n\n Die " + 
+    "dynamische Programmierung ist eine systematischere Methode, die alle möglichen Kombinationen von Objekten " + 
+    "analysiert und die optimalen Lösungen für Teilprobleme speichert. Dieser Ansatz garantiert eine optimale " + 
+    "Lösung, ist jedoch rechenintensiver.\n\nBobs Aufgabe ist es in diesem Level, einen möglichst hohen Wert in " + 
+    "seinen Beutel zu stopfen. Bedenke, dass er nur begrenzt Beute tragen kann, da er noch fliehen muss!";
 }

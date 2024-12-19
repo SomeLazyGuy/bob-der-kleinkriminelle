36d41c623cc6de118cca6953ccd6bb5e196f5320
SomeLazyGuy <konstantin.kilbel@gmail.com>
2024-12-19 11:23:44 +0100

    Add intro animation

--- BobDerKleinkriminelle/Assets/Scripts/MenuUIController.cs
+++ BobDerKleinkriminelle/Assets/Scripts/MenuUIController.cs
@@ -1,7 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class MenuUIController : MonoBehaviour {
+    [SerializeField] private VideoPlayer introPlayer;
+    [SerializeField] private Image introThumb;
+    [SerializeField] private Button playButton;
+
+    private void Start() {
+        introPlayer.loopPointReached += EndReached;
+    }
+    
     public void StartGame() {
+        introPlayer.Play();
+        introThumb.gameObject.SetActive(false);
+        playButton.gameObject.SetActive(false);
+    }
+
+    private void EndReached(VideoPlayer vp) {
         LevelManager.Instance.SwitchScene("Level1");
     }
 }

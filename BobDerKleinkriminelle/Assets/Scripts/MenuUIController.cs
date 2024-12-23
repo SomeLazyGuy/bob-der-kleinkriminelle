adec39b02f08ea8ddfbf3308063d39ade6e8edb5
SomeLazyGuy <konstantin.kilbel@gmail.com>
2024-12-23 14:00:57 +0100

    Update main menu

--- BobDerKleinkriminelle/Assets/Scripts/MenuUIController.cs
+++ BobDerKleinkriminelle/Assets/Scripts/MenuUIController.cs
@@ -5,16 +5,27 @@ using UnityEngine.Video;
 public class MenuUIController : MonoBehaviour {
     [SerializeField] private VideoPlayer introPlayer;
     [SerializeField] private Image introThumb;
-    [SerializeField] private Button playButton;
+    [SerializeField] private GameObject text;
+
+    private bool _starting;
 
     private void Start() {
         introPlayer.loopPointReached += EndReached;
+    }
+
+    private void Update() {
+        if (_starting) return;
+        
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            _starting = true;
+            StartGame();
+        }
     }
     
     public void StartGame() {
         introPlayer.Play();
         introThumb.gameObject.SetActive(false);
-        playButton.gameObject.SetActive(false);
+        text.gameObject.SetActive(false);
     }
 
     private void EndReached(VideoPlayer vp) {
